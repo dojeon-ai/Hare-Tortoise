8857de0baf70428b8e2752ff2e74c452bff9ec0b
dmslab-nlp03 <chohs9803@gmail.com>
2024-05-31 17:26:33 +0900

    add methods

--- scripts/paper/warm_start_wo_aug/spectral.sh
+++ scripts/paper/warm_start_wo_aug/spectral.sh
@@ -3,7 +3,7 @@ cd ../../../
 
 spectral_rates=(1.0 0.1 0.01 0.001 0.0001 0.00001)
 configs=("cifar10_resnet18" "cifar100_vit-ti" "tig_vgg16")
-
+ 
 for seed in {1..5}; do
     for config in "${configs[@]}"; do
         for spectral in "${spectral_rates[@]}"; do
